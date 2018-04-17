bb47c935c4ab8d5fb03d8587c745173ee4e8affc
Philippe Dugre <pdugre@devolutions.net>
2018-04-17 13:32:50 -0400

    Length of blob type is now padded with the header. Removed the 8-bytes padding at the end of the blob

--- src/message_types/srd_blob.rs
+++ src/message_types/srd_blob.rs
@@ -51,10 +51,6 @@ impl SrdBlobInterface for SrdBlob {
         let mut padding = vec![0u8; data_padding as usize];
         buffer.read_exact(&mut padding)?;
 
-        // Needed to be a multiple of 16
-        let mut padding = vec![0u8; 8];
-        buffer.read_exact(&mut padding)?;
-
         Ok(SrdBlob { blob_type, data })
     }
 
@@ -62,7 +58,7 @@ impl SrdBlobInterface for SrdBlob {
         let mut rng = OsRng::new()?;
 
         let type_size = self.blob_type.len() + 1;
-        let type_padding = 16 - (type_size % 16);
+        let type_padding = 16 - (type_size + 8) % 16;
         let data_size = self.data.len();
         let data_padding = 16 - (data_size % 16);
 
@@ -81,11 +77,6 @@ impl SrdBlobInterface for SrdBlob {
         buffer.write_all(&self.data)?;
 
         let mut padding = vec![0u8; data_padding];
-        rng.fill_bytes(&mut padding);
-        buffer.write_all(&padding)?;
-
-        // Needed to be a multiple of 16
-        let mut padding = vec![0u8; 8];
         rng.fill_bytes(&mut padding);
         buffer.write_all(&padding)?;
 
@@ -111,32 +102,3 @@ fn blob_encoding() {
         Err(_) => assert!(false),
     };
 }
-
-/*fn convert_and_pad_to_cstr(rng: &mut OsRng, str: &str) -> Result<[u8; 128]> {
-    //TODO: Block large username and password
-    let mut cstr = [0u8; 128];
-    std::ffi::CString::new(str)?
-        .as_bytes_with_nul()
-        .read(&mut cstr)?;
-    let index = match cstr.iter().enumerate().find(|&x| *x.1 == b'\x00') {
-        None => {
-            return Err(SrdError::InvalidCstr);
-        }
-        Some(t) => t.0,
-    };
-    for i in index + 1..cstr.len() {
-        cstr[i] = rng.gen::<u8>();
-    }
-    Ok(cstr)
-}
-
-fn convert_and_unpad_from_cstr(data: &[u8]) -> Result<String> {
-    let index = match data.iter().enumerate().find(|&x| *x.1 == b'\x00') {
-        None => {
-            return Err(SrdError::InvalidCstr);
-        }
-        Some(t) => t.0,
-    };
-    Ok(String::from_utf8(data[0..index].to_vec())?)
-}
-*/
