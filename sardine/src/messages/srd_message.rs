fb76c4be0542e8758dd01486d8858a2a636283b1
François Dubois <fdubois@devolutions.net>
2018-08-03 11:21:38 -0400

    WAYK-1163 - Removed useless comments

--- sardine/src/messages/srd_message.rs
+++ sardine/src/messages/srd_message.rs
@@ -194,23 +194,6 @@ pub trait ReadMac {
     fn read_mac(&mut self, mac: &mut [u8]) -> Result<()>;
 }
 
-//impl<'a> ReadMac for std::io::Cursor<&'a [u8]> {
-//    fn read_mac(&mut self, mut mac: &mut [u8]) -> Result<()> {
-//
-//        // The MAC field is a footer: it is ALWAYS at the very end of the message
-//        let mac_position = match (self.get_ref().len() as u64).checked_sub(mac.len() as u64) {
-//            Some(mac_position) if mac_position >= self.position() => mac_position,
-//            _ => {
-//                // Either the buffer is not long enough or not enough data is available to read.
-//                return Err(SrdError::InvalidDataLength);
-//            }
-//        };
-//        self.set_position(mac_position);
-//        self.read_exact(&mut mac)?;
-//        Ok(())
-//    }
-//}
-
 impl<T: Read> ReadMac for T {
     fn read_mac(&mut self, mac: &mut [u8]) -> Result<()> {
         let mut v = Vec::new();
